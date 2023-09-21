21d54e00ede70e386ff534635d4ed7dc2dd9b2f0
fabiante <fabiante@users.noreply.github.com>
2023-09-21 12:34:37 +0200

    doc: document oidc package

--- doc.go
+++ doc.go
@@ -1,1 +1,5 @@
+// Package oidc provides an easy-to-use client (RP) and server (OP) implementation
+// for the OIDC (OpenID Connect) standard.
+//
+// For details visit https://github.com/zitadel/oidc
 package oidc
